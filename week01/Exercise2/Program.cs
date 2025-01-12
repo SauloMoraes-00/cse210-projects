e1664175085e91c1f40020221fb26ca66a0aa984
SauloMoraes-00 <saulo.moraesoficial@gmail.com>
2025-01-11 22:18:08 -0300

    All done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your Grade Persentege? ");
+        string gradeNumb = Console.ReadLine();
+        int gradePerc = int.Parse(gradeNumb);
+
+        string letter = "";
+
+        if ((gradePerc == 90) || gradePerc > 90)
+        {
+            letter = "A";
+        }
+
+        else if ((gradePerc == 80) || gradePerc > 80)
+        {
+            letter = "B";
+        }
+
+        else if ((gradePerc == 70) || gradePerc > 70)
+        {
+            letter = "C";
+        }
+        else if ((gradePerc == 60) || gradePerc > 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (gradePerc >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sadly, you failed.");
+        }
+
     }
 }
