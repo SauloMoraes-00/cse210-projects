bdc4ba96f1e79a0f4a7e31d7de64e4316f51586d
SauloMoraes-00 <saulo.moraesoficial@gmail.com>
2025-02-17 12:16:28 -0300

    Exerc tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace FitnessTracker
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            // Create activities
+            Running running = new Running(new DateTime(2023, 10, 1), 30, 4.8);
+            Cycling cycling = new Cycling(new DateTime(2023, 10, 2), 45, 20.0);
+            Swimming swimming = new Swimming(new DateTime(2023, 10, 3), 60, 40);
+
+            // Add activities to a list
+            List<Activity> activities = new List<Activity>
+            {
+                running,
+                cycling,
+                swimming
+            };
+
+            // Display summaries
+            foreach (var activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
